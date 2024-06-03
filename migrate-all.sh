597ec7f5de9a731ab6735230695af40127042297
Manuel Rabade <manuel@rabade.net>
2024-06-02 21:04:51 -0600

    Migrate all podcasts to AntennaPod

---
+++ migrate-all.sh
@@ -0,0 +1,9 @@
+php migrate_to_antennapod.php google_podcasts_html/Cleaning\ Up.html https://feeds.simplecast.com/j3au6_cC
+php migrate_to_antennapod.php google_podcasts_html/Cold\ War\ Conversations.html https://feeds.megaphone.fm/NSR5326520675
+php migrate_to_antennapod.php google_podcasts_html/Darknet\ Diaries.html https://podcast.darknetdiaries.com
+php migrate_to_antennapod.php google_podcasts_html/Docuhistoria.html https://anchor.fm/s/2fc10d48/podcast/rss
+php migrate_to_antennapod.php google_podcasts_html/Engineering\ Enablement.html https://feeds.transistor.fm/the-engineering-enablement-podcast
+php migrate_to_antennapod.php google_podcasts_html/Radio\ Ambulante.html https://feeds.npr.org/510315/podcast.xml
+php migrate_to_antennapod.php google_podcasts_html/Stuff\ You\ Missed\ in\ History\ Class.html https://www.omnycontent.com/d/playlist/e73c998e-6e60-432f-8610-ae210140c5b1/cfb428ef-eafc-44d0-9d09-ae2701747e6f/fb626e1f-112c-4246-a40d-ae2701747e7d/podcast.rss
+php migrate_to_antennapod.php google_podcasts_html/Stuff\ You\ Should\ Know.html https://www.omnycontent.com/d/playlist/e73c998e-6e60-432f-8610-ae210140c5b1/a91018a4-ea4f-4130-bf55-ae270180c327/44710ecc-10bb-48d1-93c7-ae270180c33e/podcast.rss
+php migrate_to_antennapod.php google_podcasts_html/The\ Changelog.html https://changelog.com/master/feed
